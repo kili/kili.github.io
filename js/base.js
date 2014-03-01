8c8e1ef164fc8ee5ab64fa873e08d4febe125579
James Nzomo <kazikubwa@gmail.com>
2014-03-01 20:23:43 +0300

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,4 +1,4 @@
-function scrollToA(target) { console.log("qwert"); $('html').animate({ scrollTop: $(target).offset().top }, 1000); return true; }
+function scrollToA(target) { $('html').animate({ scrollTop: $(target).offset().top }, 1000); return true; }
 
 var EXCHANGE_RATE = 86;
 
