1b8f0817d59efb1fd6d7d3a00b20caf4a6938189
James Nzomo <kazikubwa@gmail.com>
2014-03-17 16:42:28 +0300

    Moved some inline js form index.html to base.js

--- js/base.js
+++ js/base.js
@@ -1,4 +1,7 @@
-function scrollToA(target) { $('html, body').animate({ scrollTop: $(target).offset().top }, 1000); return true; }
+function scrollToA(target) { 
+    $('html, body').animate({ scrollTop: $(target).offset().top }, 1000); 
+    return true; 
+}
 
 var EXCHANGE_RATE = 86;
 
@@ -15,3 +18,28 @@ function swapCurr() {
 }
 
 $("#currSwitch > b > a").on("click", function(e){ swapCurr(); });
+
+swapCurr();
+$(document).ready(function() {
+    $('.carousel').carousel({ interval: false });
+    $("[rel=tooltip]").tooltip();
+    $('.tabbable a').click(function (e) {
+            e.preventDefault();
+            $(this).tab('show');
+        });
+
+        $('.toggle').toggles({text:{on:'USD', off:'KES'}, width:110, height:40 , type:'select'});
+        $('.toggle').on('toggle', function (e, active) { swapCurr(); 
+    });
+
+    // navigation scrolling
+    function scrollHere(target, end){
+        $(target).on('click', function(e){
+            e.preventDefault();
+            $.scrollTo( $(end).offset().top-69, 600)
+        })
+    }
+    scrollHere('#features-nav', '#features')
+    scrollHere('#pricing-nav', '#pricing')
+    scrollHere('#cta-nav', '#cta')
+});
